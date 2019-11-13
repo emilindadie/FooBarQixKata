e457ea242dbfa85f7189ff5a896fe854ccaddce8
emilindadie <dadie.emilin@gmail.com>
2019-11-13 12:16:53 +0100

    unit test contains OK case

--- src/foobarqix.js
+++ src/foobarqix.js
@@ -1,9 +1,25 @@
 
 
 const FooBarQix = {
-    
+    defaultValue : function (){
+        return [
+            ["3", "Foo"],
+            ["5", "Bar"],
+            ["7", "Qix"]
+        ];
+    },
+
     containsFooBarQix : function(string){
-       return "";
+        let tabString = this.defaultValue().map(value => {
+            return value[1];
+        });
+        tabString.push(tabString.join(''));
+        let contains = false;
+        tabString.forEach(value => {
+            if(value === string)
+            contains = true;
+        })
+        return contains;
     },
 }
 
