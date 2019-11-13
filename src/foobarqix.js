18289aeec615f79b08f78d30142c404620a5516c
emilindadie <dadie.emilin@gmail.com>
2019-11-13 14:58:59 +0100

    unit test should compute OK case + fixed unit test contains because its not work for all case

--- src/foobarqix.js
+++ src/foobarqix.js
@@ -9,19 +9,24 @@ const FooBarQix = {
         ];
     },
 
+    // This function take a string and check if the string contains Foo Bar or Qix
+    // Its will return true if the string contains Foo or Bar or Qix
     containsFooBarQix : function(string){
         let tabString = this.defaultValue().map(value => {
             return value[1];
         });
-        tabString.push(tabString.join(''));
-        let contains = false;
-        tabString.forEach(value => {
-            if(value === string)
-            contains = true;
-        })
-        return contains;
+        const specialCarac = '\\';
+        const regex = "".concat(specialCarac, tabString.join('|\\', 'g')); 
+        if(string.match(regex)){
+            return true;
+        }
+        return false;
     },
 
+
+
+    // This function take a number of type String and check if the number is divisible by 3 and return "Foo", by 5 and return "bar", by 7 and return "Qix"
+    // Its will return the number in params if its not divisible by 3, 5 and 7
     divisible : function(number){
         const divisibleResult = this.defaultValue().map(item => {
             return number % item[0] == 0 ? item[1] : ""; 
@@ -29,6 +34,9 @@ const FooBarQix = {
         return this.containsFooBarQix(divisibleResult.join(''))? divisibleResult.join('') : number;
     },
 
+
+    // This function take a number of type String in params
+    // For each 3 , 5, or 7 that will be founded, this one add "Foo" for 3, "Bar" for 5 and "Qix" for 7 in digits order
     completeEachDigit : function(number){
         let completeDigit = "";
         number.split('').forEach(caract => {
@@ -41,12 +49,29 @@ const FooBarQix = {
         return completeDigit;
     },
 
+    // This function take a String in params
+    // this check all digits for replace 0 by *
     addSpecialCaracterForeach0 : function(string){
         return string.replace(new RegExp(0, 'g'), '*');
     },
 
+
+    // This function take a number of type String in params
+    // this function is the implementation of FooBarQuix kata (Combinasion of all itération is use in this function)
     compute : function(number){
-        return "";
+        let tab = [];
+        tab.push(this.divisible(number));
+        number.split('').map(letter => {
+            tab.push(this.completeEachDigit(letter));
+            if(letter === "0"){
+                let tmpTab = this.addSpecialCaracterForeach0(tab.join('').concat("0"));
+                tab = [];
+                tab.push(tmpTab);
+            }
+        });
+        const containsFooBarQix = this.containsFooBarQix(tab.join(''));
+        const computeValue = containsFooBarQix? tab.join('') : number;
+        return computeValue;
     },
 }
 
--- tests/compute.js
+++ tests/compute.js
@@ -6,6 +6,19 @@ describe('FooBarQix compute Test', () => {
     it('should compute step1', () => {
         // Arrange 
         const foobarQix = Object.create(FoobarQix);
+        const input = "15";
+        let output = "";
+    
+        // Act
+        output = foobarQix.compute(input);
+
+        //Assert
+        expect(output).toBe("FooBarBar");
+    });
+
+    it('should compute step1', () => {
+        // Arrange 
+        const foobarQix = Object.create(FoobarQix);
         const input = "33";
         let output = "";
     
@@ -14,5 +27,18 @@ describe('FooBarQix compute Test', () => {
 
         //Assert
         expect(output).toBe("FooFooFoo");
+    });
+
+    it('should compute step2', () => {
+        // Arrange 
+        const foobarQix = Object.create(FoobarQix);
+        const input = "303";
+        let output = "";
+    
+        // Act
+        output = foobarQix.compute(input);
+
+        //Assert
+        expect(output).toBe("FooFoo*Foo");
     });
   });
